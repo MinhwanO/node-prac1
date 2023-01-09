3a0f5344b185df961fec8e6b5591653ed733d287
DESKTOP-TILHQ4P\user <lmhnote@naver.com>
2023-01-09 15:49:35 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 5000
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const { User } = require('./models/User');
+
+app.use(express.json()); //For JSON requests, application/json 분석해서 가져올수 있도록
+app.use(express.urlencoded({extended: true}));//application/x-www-form-urlencoded 분석해서 가져올수 있도록
+
+
+
 
 // 몽고db 연결
 mongoose
@@ -12,6 +19,15 @@ mongoose
 
 app.get('/', (req, res) => {
     res.send('안녕하세요')
+})
+
+app.post('/api/users/register', (req, res) => {
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({success: false, err})
+        return res.status(200).json({success: true})
+    })
 })
 
 app.listen(port, () => {
